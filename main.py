c48288abe0e2e928f3f42d8d8170001093c4261d
saraivx <joaogabrielssaraiva@gmail.com>
2025-11-10 19:10:34 -0300

    updating data extracting.

--- main.py
+++ main.py
@@ -6,22 +6,24 @@ import time
 
 sleep_time = 30
 now = datetime.now()
+df = pd.DataFrame(pd.read_csv("url_links.csv", header=None, index_col=False))
+url_list = df.iloc[1:]
 
 def scrap_data():
-    url = "https://sb2frontend-altenar2.biahosted.com/api/widget/GetLiveEvents?culture=pt-BR&timezoneOffset=180&integration=mcgames2&deviceType=1&numFormat=en-GB&countryCode=BR&eventCount=0&sportId=0&champIds=11318"
-    response = req.get(url)
-    json_data = response.json()
-    df = pd.json_normalize(json_data)
-    print("====================================")
-    print("Web scraping from MCGames.")
-    print(f"Scrapping response: {response.status_code}")
-    df.to_json(f"raw_data/{str(datetime.now())}.json")
-    print("The data is sucessfully scraped. =D")
-    print("====================================")
+    for i, row in url_list.iterrows(): 
+        response = req.get(row[0])
+        json_data = response.json()
+        df = pd.json_normalize(json_data)
+        print("====================================")
+        print(f"Web scraping from url='{row[0]}.'")
+        print(f"Scrapping response: {response.status_code}")
+        df.to_json(f"raw_data/{str(datetime.now().strftime("%Y-%m-%d_%H:%M:%S"))}.json")
+        print("The data is sucessfully scraped. =D")
+        print("====================================")
 
-
-while True:
-    scrap_data()
-    for i in range(sleep_time, 0, -1):
-        print(f"The next scrap will be done on {i} seconds...")
-        time.sleep(1)
+scrap_data()
+# while True:
+#     scrap_data()
+#     for i in range(sleep_time, 0, -1):
+#         print(f"The next scrap will be done on {i} seconds...")
+#         time.sleep(1)
